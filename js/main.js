755f0cd086fa4f211be95efa46441fb0df710e34
Warstainer <one_i@mail.ru>
2016-04-25 21:11:49 +0300

    some buttons minor changes plus new column work

--- js/main.js
+++ js/main.js
@@ -7,4 +7,11 @@ $(".articles").mouseout(function() {
 	$(".paragraph").css({"transition":"all .4s ease-in-out","padding-top":"3em"});
 	})
 
+// laser beam
+// $(".learnMore").mouseover(function (){
+// 	$(this).html("Laser Beam!")
+// });
+// $(".learnMore").mouseout(function (){
+// 	$(this).html("Learn More!")
+// });
 })
